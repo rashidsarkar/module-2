f3ba9836c61d80fa3e3c0a6d5e14052c706f38b8
rashidsarkar <rashidrock558@gmail.com>
2026-04-19 19:42:13 +0600

    moduke 3 staty

---
+++ app.py
@@ -0,0 +1,14 @@
+# with open('./sample_data/test.txt','r') as file:
+#   content = file.readlines()
+#   content = list(map(str.strip,content))
+#   # print(content)
+#   filter_content = filter(lambda x : len(x)<1 ,content)
+#   # print(list(filter_content))
+#   # print(list(filter(lambda x : len(x)<1 ,content)))
+#   print(list(filter_content))
+
+# strings =['hello6','world6','python6','is6','awesome5']
+# with open('./sample_data/test2.txt','a') as file:
+    # file.writelines(strings)
+#               file pointer module start 
+
